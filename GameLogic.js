9f06a91ee4b0d7aa1f60ddedf3b948c7b42236a6
Giovanni Hsu <giovannihsu.123@gmail.com>
2021-10-23 00:02:09 -0400

    modified init and print

--- GameLogic.js
+++ GameLogic.js
@@ -9,14 +9,16 @@ function intializeGrid() {
     }
     return grid;
 }
+const numToIcon = {0:" ", 1:"#", 2:"o", 3:"o"};
 function printGrid(grid) {
-    const SIZE = grid.length;
-    for(var i=0; i<SIZE; i++) {
-        for(var j=0; j<SIZE; j++) {
-            process.stdout.write(grid[i][j] + " ");
+    process.stdout.write("----------------\n");
+    for(var i=0; i<grid.length; i++) {
+        for(var j=0; j<grid[i].length; j++) {
+            process.stdout.write(numToIcon[grid[i][j]] + " ");
         }
         process.stdout.write("\n");
     }
+    process.stdout.write("----------------\n");
 }
 // console.log(intializeGrid())
 printGrid(intializeGrid())
