c962edc9b944b66edae6dbbe12fafbdc03437f37
Mantelopes <marioneveslopes@gmail.com>
2021-10-23 18:57:10 -0400

    partial process function

---
+++ src/Algorithm.js
@@ -0,0 +1,9 @@
+function process(waterStatusArray, blockArray) {
+	var newWaterStatusArray = new Array(waterStatusArray.length);
+	for (let index = 0; index < blockArray.length, index++) {
+		if (blockArray[index] === 0) {
+			
+		}
+	}
+	return newWaterStatusArray;
+}
