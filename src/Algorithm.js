77f27c3ec2b2acb70435290c75e3ca1de21f7d86
Mantelopes <marioneveslopes@gmail.com>
2021-10-23 19:46:12 -0400

    process function

--- src/Algorithm.js
+++ src/Algorithm.js
@@ -1,8 +1,24 @@
 function process(waterStatusArray, blockArray) {
 	var newWaterStatusArray = new Array(waterStatusArray.length);
-	for (let index = 0; index < blockArray.length, index++) {
-		if (blockArray[index] === 0) {
-			
+	var change = true;
+	while (change) {
+		change = false;
+		for (let index = 0; index < blockArray.length; index++) {
+			if (waterStatusArray[index] === 0 && index - 1 !== -1 && waterStatusArray[index-1] === 1 && blockArray[index-1] === 1) {
+				waterStatusArray[index] = 1;
+				change = true;
+			}
+			if (waterStatusArray[index] === 0 && index + 1 !== blockArray.length && waterStatusArray[index+1] === 1 && blockArray[index+1] === 1) {
+				waterStatusArray[index] = 1;
+				change = true;
+			}
+		}
+	}
+	for (let indexx = 0; indexx < blockArray.length; indexx++) {
+		if (blockArray[indexx] === 0 && waterStatusArray[indexx] === 1) {
+			newWaterStatusArray[indexx] = 1;
+		} else {
+			newWaterStatusArray[indexx] = 0;
 		}
 	}
 	return newWaterStatusArray;
